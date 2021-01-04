cdd05b5cd2f3e40b9f75a662719ead13afcdbe1f
Crowdbotics <team@crowdbotics.com>
2021-01-04 08:58:51 +0000

    Update components

--- src/features/BlankScreen0189800/screens/blank.js
+++ src/features/BlankScreen0189800/screens/blank.js
@@ -24,9 +24,9 @@ export class Blank extends React.Component {
   render = () => (
     <View style={styles.View_1}>
       <View style={styles.View_5}>
-        <Text>Sample text content 1</Text>
-        <Text>Sample text content 2</Text>
-        <Text>Sample text content 3</Text>
+        <Text style={styles.Text_91}>Sample text content 1</Text>
+        <Text style={styles.Text_95}>Sample text content 2</Text>
+        <Text style={styles.Text_99}>Sample text content 3</Text>
       </View>
       <View style={styles.View_7}>
         <View>
@@ -49,23 +49,36 @@ export class Blank extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  View_1: { width: 0, flexDirection: "row", alignContent: "flex-start" },
+  View_1: {
+    width: 0,
+    fontFamily: "Roboto-Regular",
+    flexDirection: "row",
+    alignContent: "flex-start"
+  },
   View_5: {
     width: "20%",
     height: 100,
-    borderColor: "#d1f4ff",
-    borderStyle: "dotted"
+    color: "#ffffff",
+    backgroundColor: "#94d6ff",
+    fontFamily: "Roboto-Regular",
+    borderColor: "#ffffff",
+    borderStyle: "solid",
+    borderWidth: 2,
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
   },
-  Text_91: {},
-  Text_95: {},
-  Text_99: {},
-  View_7: { width: "60%" },
+  Text_91: { backgroundColor: "#94d6ff" },
+  Text_95: { backgroundColor: "#94d6ff" },
+  Text_99: { backgroundColor: "#94d6ff" },
+  View_7: { width: "60%", backgroundColor: "#f5faff" },
   View_18: {},
   Text_37: {},
   TextInput_36: {},
   View_22: {},
   Button_41: {},
-  View_9: { width: "20%" }
+  View_9: { width: "20%", color: "#ffffff", backgroundColor: "#70a7ff" }
 })
 
 function mapStateToProps(state) {
